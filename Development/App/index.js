2b81e91ba2ceedb2ca9be8f8bbff202f010cb5b3
Elman Seferli <itiselman@gmail.com>
2025-07-07 11:52:48 +0400

    Preloader

---
+++ Development/App/index.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  // Preloader
+  $("html").addClass("preloader-active");
+
+  $(window).on("load", function () {
+    setTimeout(function () {
+      $("#preloader").fadeOut(500, function () {
+        $("html").removeClass("preloader-active");
+      });
+    }, 1000);
+  });
+
+  $(document).on("pjax:start", function () {
+    $("#preloader").fadeIn(400);
+    $("html").addClass("preloader-active");
+  });
+
+  $(document).on("pjax:end", function () {
+    setTimeout(function () {
+      $("#preloader").fadeOut(500, function () {
+        $("html").removeClass("preloader-active");
+      });
+    }, 1000);
+  });
+
+  // Solution Panel
+  if ($(".sptm_bottom").length > 0) {
+    $(".sptm_bottom").hide();
+    $(".solution_panel:first .sptm_bottom")
+      .addClass("activeSolutionPanel")
+      .show();
+
+    $(".solution_panel").on("click", function () {
+      $(".sptm_bottom").removeClass("activeSolutionPanel").slideUp(300);
+      $(this)
+        .find(".sptm_bottom")
+        .addClass("activeSolutionPanel")
+        .slideDown(300);
+    });
+  } else {
+    console.warn("Some elements are missing.");
+  }
+});
